c0e2c8a5f20c6658348bf24111f7a2c9e9b87bf4
mapsacosta <macosta@fnal.gov>
2018-06-14 23:59:19 +0200

    Parsed ES output

--- metrics/ftslinks/getlinks.py
+++ metrics/ftslinks/getlinks.py
@@ -4,8 +4,24 @@ import sys, os
 import json
 import pprint
 
-endpoints = sites.getSRMEndpoints()
-for site in endpoints:
-  print site
-  print sites.getTier(site)
-  print endpoints[site]
+#endpoints = sites.getSRMEndpoints()
+#for site in endpoints:
+  #print site
+  #print sites.getTier(site)
+  #print endpoints[site]
+
+with open('data.json') as f:
+  data = json.load(f)
+
+#for key, value in data.iteritems() :
+#      pprint.pprint(value)
+
+for tier in data['responses']:
+  for tag in tier['aggregations']['source']['buckets']:
+    pprint.pprint('--------------------- '+ tag['key']+ '---------------------')
+    for dest in tag['dest']['buckets']:
+      pprint.pprint('-->' + dest['key'])
+      pprint.pprint(dest['doc_count'])
+      for fts in dest['filestates']['buckets']:
+        print fts['key']+', '+str(fts['doc_count'])
+        
