86fd7403e8b41ab74c42f0fb3a513e05da6b30a6
mapsacosta <macosta@fnal.gov>
2018-07-27 21:09:39 +0200

    Cleaning old script, changes to init.sh

--- metrics/ftslinks/init.sh
+++ metrics/ftslinks/init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-export SSTBASE=/data/cmssst/MonitoringScripts/
+export SSTBASE=/afs/cern.ch/user/m/macostaf/private/MonitoringScripts/
 export CERTS=$SSTBASE/certs
 export PYTHONPATH="$PYTHONPATH:$SSTBASE"
 export SSTOUT="$SSTBASE/output"
 export SSTLOG="$SSTBASE/log"
-export SSTMAIL="cms-comp-ops-site-support-team@cern.ch"
+#export SSTMAIL="cms-comp-ops-site-support-team@cern.ch"
+export SSTMAIL="macostaf@cern.ch"
 SSTINITERR="$SSTLOG/sstInit.log"
 
 if [ ! -d "$SSTOUT" ]; then
